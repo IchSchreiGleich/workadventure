853e187bc5f66cc341a9c1807d3037e12ef7d2b0
marco stoeckert <marco.stoeckert@quentic.com>
2023-11-17 17:10:38 +0100

    bla

---
+++ scripts/quentic.js
@@ -0,0 +1,20 @@
+import { } from "https://unpkg.com/@workadventure/scripting-api-extra@^1";
+
+let helloWorldPopup;
+
+// Open the popup when we enter a given zone
+WA.room.onEnterZone("fridge").subscribe(() => {
+    helloWorldPopup = WA.ui.openPopup("popupRectangle", 'Hello world!', [{
+        label: "Close",
+        className: "primary",
+        callback: (popup) => {
+            // Close the popup when the "Close" button is pressed.
+            popup.close();
+        }
+    }]);
+});
+
+// Close the popup when we leave the zone.
+WA.room.onEnterZone("fridge").subscribe(() => {
+    helloWorldPopup.close();
+})
